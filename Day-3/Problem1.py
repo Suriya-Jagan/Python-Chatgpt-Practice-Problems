2f47debd331879e28a5fd9dc8724ca3d0275546e
Suriya-Jagan <suriya29102000@gmail.com>
2023-10-26 12:46:13 +0530

    Modified

---
+++ Day-3/Problem1.py
@@ -0,0 +1,10 @@
+# Problem 1:
+# Write a function that takes a list of numbers and returns the sum of all the even numbers in the list.
+def sumevenno(l):
+    l1 = l.split()
+    l2 = []
+    for i in l1:
+        l3 = int(l1)
+        l2.append(l3)
+    print(l2)
+print(sumevenno(10 20 30 40 50))
