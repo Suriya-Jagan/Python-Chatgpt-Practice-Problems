fa0716e4ccc2d6026b2aacc30b1c492bc1bca261
Suriya-Jagan <suriya29102000@gmail.com>
2023-10-27 12:02:34 +0530

    Corrected

--- Day-3/Problem1.py
+++ Day-3/Problem1.py
@@ -1,10 +1,27 @@
 # Problem 1:
 # Write a function that takes a list of numbers and returns the sum of all the even numbers in the list.
 def sumevenno(l):
-    l1 = l.split()
+    l1 = l.split(",")
     l2 = []
+    sumeven = 0
     for i in l1:
-        l3 = int(l1)
+        l3 = int(i)
         l2.append(l3)
-    print(l2)
-print(sumevenno(10 20 30 40 50))
+    
+    for i in l2:
+        if i % 2 == 0:
+            sumeven += i
+        else:
+            pass
+    return sumeven
+
+x = sumevenno("10,25,30,37,42,50,59,64")
+print(x)
+
+#Method 2:
+def sum_even_no(input_string):
+    numbers = input_string.split(",")
+    even_numbers = [int(i) for i in numbers if int(i) % 2 ==0]
+    return sum(even_numbers)
+x = sum_even_no("10,25,30,37,42,50,59,64")
+print(x)
