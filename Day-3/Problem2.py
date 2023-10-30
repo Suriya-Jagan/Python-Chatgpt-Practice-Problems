3dc4fa44ba65757aeacf185e5189cde7b2886374
Suriya-Jagan <suriya29102000@gmail.com>
2023-10-30 19:28:38 +0530

    Created

---
+++ Day-3/Problem2.py
@@ -0,0 +1,22 @@
+# Create a function that takes a string as input and returns the reverse of the string 
+# without using slicing or the reverse() method.
+
+def rev_string(inputsring):
+    l = len(inputsring)
+    output = []
+    for i in range(l-1,-1,-1):
+        x = inputsring[i]
+        output.append(x)
+    
+    rev = "".join(output)
+    return rev
+print(rev_string("Pasta"))
+
+
+#Method2
+def revstring(inputstring):
+    rev = ""
+    for i in inputstring:
+        rev = i + rev
+    return rev
+print(revstring("Pastor"))
