a0bb953363de54dc04127ffbf7b9cfdc73c8f10c
Suriya-Jagan <suriya29102000@gmail.com>
2023-11-01 01:56:26 +0530

    Created

---
+++ Day-5/Problem2.py
@@ -0,0 +1,5 @@
+# Problem 2:
+# Write a function that checks if a string is a palindrome. The function should be 
+# case-insensitive and should ignore spaces and punctuation.
+
+
---
+++ Day-5/Python1.py
@@ -0,0 +1,8 @@
+# Problem 1:
+# Create a function that takes a string as input and returns the number of words in the string.
+
+def find_len_of_string(input_string):
+    return len(input_string)
+input_string = input("Enter the string: ")
+x = find_len_of_string(input_string)
+print(x)
