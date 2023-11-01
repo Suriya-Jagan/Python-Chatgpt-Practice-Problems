e8f5a63bdd3e4fe57321d53354845b0721530086
Suriya-Jagan <suriya29102000@gmail.com>
2023-11-01 16:06:10 +0530

    Modified

--- Day-5/Problem1.py
+++ Day-5/Problem1.py
@@ -2,7 +2,8 @@
 # Create a function that takes a string as input and returns the number of words in the string.
 
 def find_len_of_string(input_string):
-    return len(input_string)
+    words = input_string.split()
+    return len(words)
 input_string = input("Enter the string: ")
 x = find_len_of_string(input_string)
 print(x)
