437f238008b0b15031949412ed6193bb1c7df4ee
Suriya-Jagan <suriya29102000@gmail.com>
2023-11-01 16:06:45 +0530

    Modified

--- Day-5/Problem5.py
+++ Day-5/Problem5.py
@@ -2,11 +2,6 @@
 # Create a function that takes a list of strings and returns a new list with 
 # all the strings sorted in alphabetical order.
 
-# x = "pasta"
-# u = sorted(x)
-# x = "".join(u)
-# print(x)
-
 def sorted_string_list(string_list):
     sorted_list = []
     for i in string_list:
